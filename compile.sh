34f731d3218fa03e1076d9113b0991c53573ec92
nicolosponziello <nicolosponziello@gmail.com>
2019-07-28 08:28:53 +0200

    Update compile script with gcc 9.1.0 toolchain

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 #export CROSS_COMPILE=$(pwd)/aarch64-linux-gnu/bin/aarch64-linux-gnu-
 #export CROSS_COMPILE_ARM32=$(pwd)/aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
-export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+#export CROSS_COMPILE_ARM32=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
+export CROSS_COMPILE=$(pwd)/aarch64/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=$(pwd)/arm/bin/arm-eabi-
 
 export ARCH=arm64
 export SUBARCH=arm64
@@ -15,11 +17,11 @@ make O=out mrproper
 make O=out gemini_defconfig
 make O=out -j$(nproc --all)
 
-rm ../AnyKernel2-master/Image*
-rm ../AnyKernel2-master/kernel*
-cp out/arch/arm64/boot/Image.gz-dtb ../AnyKernel2-master/
+rm ../AnyKernel3/Image*
+rm ../AnyKernel3/kernel*
+cp out/arch/arm64/boot/Image.gz-dtb ../AnyKernel3/
 
-cd ../AnyKernel2-master
+cd ../AnyKernel3
 
 date=`date '+%Y-%m-%d_%H:%M'`
 
