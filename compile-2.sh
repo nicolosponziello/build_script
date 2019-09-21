2ea6d88b74798add88d6c8f102ec5a36a6605105
nicolosponziello <37869067+nicolosponziello@users.noreply.github.com>
2019-09-21 11:18:04 +0200

    Add second build script
    Different toolchain path

---
+++ compile-2.sh
@@ -0,0 +1,23 @@
+export CROSS_COMPILE=~/Desktop/kernel/aarch64/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=~/Desktop/kernel/arm/bin/arm-eabi-
+
+export ARCH=arm64
+export SUBARCH=arm64
+
+#build
+mkdir -p out
+make O=out clean
+make O=out mrproper
+make O=out gemini_defconfig
+make O=out -j$(nproc --all)
+
+rm ~/Desktop/kernel/AnyKernel3/Image*
+rm ~/Desktop/kernel/AnyKernel3/kernel*
+cp out/arch/arm64/boot/Image.gz-dtb ~/Desktop/kernel/AnyKernel3/
+
+cd ~/Desktop/kernel/AnyKernel3
+
+date=`date '+%Y-%m-%d_%H:%M'`
+date="${date//:}"
+
+zip -r9 kernel-$date.zip * -x README.md kernel-$date.zip
